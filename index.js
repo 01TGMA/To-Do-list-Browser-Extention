9ec2363c51f0caf42aadb707b558a694755c2714
01TGMA <Ghostmanhades@gmail.com>
2025-05-13 23:51:06 +0100

    checkbox strike through task  added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let saveBtn = document.getElementById("Saver")
 let onList = document.getElementById("disp")
 let deleteEl = document.getElementById("delete")
 
+
 saveBtn.addEventListener( "click", function(){
     lisT.push(inputB.value)
     inputB.value = ""
@@ -16,13 +17,13 @@ function display(){
     for(i = 0; i < lisT.length; i++ ){
         disPlay += `
         <li  style=" display: flex; gap: 8px; word-break: break-word;"> 
-             <input type="checkbox" id="to-do${i}" style=" width: 16px; height: 16px">
-                <label for="to-do${i}">${lisT[i]} </label>
+             <input type="checkbox" id="to-do${i}" class="boxCheck" style=" width: 13px; min-height: 13px; max-width: 13px; max-height: 13px;"/>
+                
+             <label for="to-do${i}" id="lab${i}" class="labText">${lisT[i]} </label>
             
         </li>`
-
-        onList.innerHTML = disPlay
     }
+    onList.innerHTML = disPlay
 }
 
 deleteEl.addEventListener("click", function(){

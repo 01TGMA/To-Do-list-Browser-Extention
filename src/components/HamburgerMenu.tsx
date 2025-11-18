522503e3b01f1b00b1e29d54f2fe81ed04107614
01TGMA <Ghostmanhades@gmail.com>
2025-11-18 02:55:32 +0100

    created span elements

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,4 +1,12 @@
 function Menu() {
-  return;
+  return (
+    <>
+      <div id="menuSpans">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  );
 }
 export default Menu;

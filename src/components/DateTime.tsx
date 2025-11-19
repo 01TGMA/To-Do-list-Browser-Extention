e43e59e25f1fd90a404106d28a77f2e7d7d6967c
01TGMA <Ghostmanhades@gmail.com>
2025-11-19 04:24:09 +0100

    created date and time component

--- src/components/DateTime.tsx
+++ src/components/DateTime.tsx
@@ -1,7 +1,27 @@
 import "../App.css";
-
+import { useEffect, useState } from "react";
 function DateTime() {
-  return <></>;
+  const [newDate, SetnewDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      SetnewDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+  const formatTime = (date: any) => {
+    const hours = date.getHours().toString();
+    const minutes = date.getMinutes().toString();
+    const seconds = date.getSeconds().toString();
+    return `${hours}:${minutes}:${seconds}`;
+  };
+  return (
+    <>
+      {" "}
+      <h2 className="time">{formatTime(newDate)}</h2>
+    </>
+  );
 }
 
 export default DateTime;
